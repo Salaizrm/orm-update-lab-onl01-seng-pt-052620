efafde2f0e0119e77c5dbdb94ad0f1436d89226d
Robert Salaiz <work.salaizrm@gmail.com>
2020-08-06 00:44:32 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -45,9 +45,6 @@ class Student
     student = Student.new(name, grade)
     student.save
   end
-
-  end
-
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
