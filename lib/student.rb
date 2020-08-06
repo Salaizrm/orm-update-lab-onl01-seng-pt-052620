ab2b5297c9f66a8ecd9d44d19f6cc03d312b430e
Robert Salaiz <work.salaizrm@gmail.com>
2020-08-06 00:51:18 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -55,7 +55,16 @@ class Student
   end
 
   def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE name = ?
+      LIMIT 1
+    SQL
 
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
   end
 
   def update
